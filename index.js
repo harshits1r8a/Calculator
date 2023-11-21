657e665404126a01e6f8c0d520907d7de590f85e
harshits1r8a <harshsharma.h8a1r3@gmail.com>
2023-11-21 13:32:07 +0530

    Update in UI

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
 const display = document.getElementById('display')
 const input = document.querySelectorAll('.input')
+const exp = document.querySelector('.exp')
+const res = document.querySelector('.res')
+let errMessage;
+// console.log(input);
 const inputArr = Array.from(input)
-console.log(inputArr);
+// console.log(inputArr);
 
-inputArr.forEach((elm)=>{
-    elm.addEventListener('click',(e)=>{
+inputArr.forEach((elm) => {
+    elm.addEventListener('click', (e) => {
         // console.log(typeof(display.innerText));
-        try{
-            if(e.target.innerText === '='){
-                let result = eval(display.innerText)
-                display.innerText = result
+        try {
+            if (e.target.innerText === '=') {
+                let result = eval(exp.innerText)
+                setTimeout(() => {
+                    res.innerText = result
+                }, 111)
             }
-            else if(e.target.innerText === 'AC'){
-                display.innerText = ''
+            else if (e.target.innerText === 'AC') {
+                exp.innerText = ''
+                res.innerText = ''
             }
-            else if(e.target.innerText === 'DEL'){
-                display.innerText = display.innerText.slice(0,-1)
+            else if (e.target.innerText === 'DEL') {
+                exp.innerText = exp.innerText.slice(0, -1)
+                res.innerText = ''
             }
-            else{
-                display.innerText += elm.innerText
+            else {
+                if(res.innerHTML != ''){
+                    exp.innerHTML = ''
+                    exp.innerHTML += res.innerHTML + elm.innerText
+                    res.innerHTML = ''
+                }else{
+                    exp.innerText += elm.innerText
+                }
             }
-        }catch(err){
-            display.innerText = 'Error'
+        } catch (err) {
+            exp.innerText = 'Error'
+            setTimeout(() => {
+                exp.innerText = ''
+                res.innerText = ''
+            }, 555)
         }
     })
 })
+
+
+
